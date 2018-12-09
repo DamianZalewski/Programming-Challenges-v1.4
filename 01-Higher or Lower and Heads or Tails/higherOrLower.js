371eae23c132f9feeb34551478f07bba81beffc3
Damian Zalewski <damian_zalewskipl@o2.pl>
2018-12-09 08:43:54 +0100

    finished first game, fixed new game issue, refactored the js code, fixed css styles.

--- 01-Higher or Lower and Heads or Tails/higherOrLower.js
+++ 01-Higher or Lower and Heads or Tails/higherOrLower.js
@@ -1,17 +1,17 @@
 let score = 0;
 let originalCards = ['2C','2D','2H','2S',
-             '3C','3D','3H','3S',
-             '4C','4D','4H','4S',
-             '5C','5D','5H','5S',
-             '6C','6D','6H','6S',
-             '7C','7D','7H','7S',
-             '8C','8D','8H','8S',
-             '9C','9D','9H','9S',
-             '10C','10D','10H','10S',
-             'JC','JD','JH','JS',
-             'QC','QD','QH','QS',
-             'KC','KD','KH','KS',
-             'AC','AD','AH','AS'];
+                     '3C','3D','3H','3S',
+                     '4C','4D','4H','4S',
+                     '5C','5D','5H','5S',
+                     '6C','6D','6H','6S',
+                     '7C','7D','7H','7S',
+                     '8C','8D','8H','8S',
+                     '9C','9D','9H','9S',
+                     '10C','10D','10H','10S',
+                     'JC','JD','JH','JS',
+                     'QC','QD','QH','QS',
+                     'KC','KD','KH','KS',
+                     'AC','AD','AH','AS'];
 let cards = [];
 originalCards.forEach((e)=>{cards.push(e)});
 let maxCards = cards.length;
@@ -19,79 +19,68 @@ let remainCards = maxCards;
 let actualCard = "";
 let isGameStarted = false;
 
-gameLogic = () => {
-    drawRemainCards();
-    drawScore();     
-}
-
-drawRemainCards = () => {
-    let scoreButton = document.getElementById("cardsRemain");
-    scoreButton.value = remainCards;
-}
-
-drawScore = () => {
-    let scoreButton = document.getElementById("gameScore");
-    scoreButton.value = score;
-}
+let gameScoreInput = document.getElementById("gameScore");
+let cardsRemainInput = document.getElementById("cardsRemain");
+let cardImg = document.getElementById("actualCard");
+let gameStatusInput = document.getElementById("gameStatus");
 
 startNewGame = () => {
-    cleanGameOver();
+    if(isGameStarted) resetCards();
     score = 0;
-    if(isGameStarted) {
-        cards = [];
-        originalCards.forEach((e)=>{cards.push(e)});
-        remainCards = maxCards;
-        
-    }
     isGameStarted = true;
-    let randomCard = Math.floor(Math.random() * cards.length);
-    actualCard = cards[randomCard] + ".jpg";
-    cards.splice(randomCard-1,1);
-    remainCards = cards.length;
+    chooseCard();
     drawCard();
     drawRemainCards();
-}
-
-drawCard = () => {
-    let cardImg = document.getElementById("actualCard");
-    cardImg.src = "assets/higherOrLower/"+actualCard;
+    drawGameStarted();
+    drawScore();
 }
 
 handleArrowClick = (position) => {
     if(!isGameStarted) return 0;
     let previousCard = actualCard;
-    let randomCard = Math.floor(Math.random() * cards.length);
-    actualCard = cards[randomCard] + ".jpg";
-    cards.splice(randomCard-1,1);
-    remainCards = cards.length;
-
+    chooseCard();
     previousValue = checkValue(previousCard);
     actualValue = checkValue(actualCard);
+    
     if(position === "up") {
-            if(actualValue >= previousValue) score += 100; else score -= 100;
+        if(actualValue >= previousValue) score += 100; else score -= 100;
     } else if(position === "down"){
         if(actualValue < previousValue) score += 100; else score -= 100;
-    }
-    
-    if(remainCards === 0) {
-        isGameStarted = false;
-        drawGameOver();
     }
     
     drawCard();
     drawRemainCards();
     drawScore();
+    
+    if(remainCards === 0) {
+        isGameStarted = false;
+        resetCards();
+        drawGameOver();
+    }
 }
 
-drawGameOver = () => {
-    let gameOverDiv = document.getElementById("gameOver");
-    gameOverDiv.innerHTML = "Game over!";
+chooseCard = () => {
+    let randomCard = Math.floor(Math.random() * cards.length);
+    actualCard = cards[randomCard] + ".jpg";
+    cards.splice(randomCard-1,1);
+    remainCards = cards.length;
 }
 
-cleanGameOver = () => {
-    let gameOverDiv = document.getElementById("gameOver");
-    gameOverDiv.innerHTML = "";
+resetCards = () => {
+    cards = [];
+    originalCards.forEach((e)=>{cards.push(e)});
+    remainCards = maxCards;
 }
+
+drawGameStarted = () => { gameStatusInput.value = "Game started!"; }
+
+drawGameOver = () => { gameStatusInput.value = "Game over!"; }
+
+drawCard = () => { cardImg.src = "assets/higherOrLower/"+actualCard; }
+
+drawRemainCards = () => { cardsRemainInput.value = remainCards; }
+
+drawScore = () => { gameScoreInput.value = score; }
 
 checkValue = (card) => {
     let cardValue = 0;
