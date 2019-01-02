09d2be39907dcb98a047816daf6242af27556cb6
DamianZalewski <damian_zalewskipl@o2.pl>
2019-01-02 22:19:35 +0100

    fixed canvas issue, added new font styles for headers, changed left menu animation structure

--- 95-Sierpinski-triangle/scripts/index.js
+++ 95-Sierpinski-triangle/scripts/index.js
@@ -1,27 +1,29 @@
 let canvas = document.getElementById('canvas');
 let canvasContainer = document.getElementById('canvasContainer');
+let counterInput = document.getElementById('counter');
 let ctx = canvas.getContext('2d');
-let cw = 800;
-let ch = 600;
+let cw = canvas.width;
+let ch = canvas.height;
 let triangleInterval;
 let points = [[cw/2,10],[10,ch-10],[cw-10,ch-10]];
 let startPoint = points[0];
 let counter = 0;
 let quantity = 0;
-
-
+let isStarted = false;
 
 init = () => {
     points = [[cw/2,10],[10,ch-10],[cw-10,ch-10]];
     startPoint = points[0];
     counter = 0;
     quantity = 0;
+    isStarted = false;
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, cw, ch);
     ctx.fillStyle = "#fff";
     ctx.fillRect(points[0][0],points[0][1],1,1);
     ctx.fillRect(points[1][0],points[1][1],1,1);
     ctx.fillRect(points[2][0],points[2][1],1,1);
+    counterInput.value = counter;
 }
 
 drawTriangle = () => {
@@ -34,30 +36,31 @@ drawTriangle = () => {
     startPoint = [destination[0], destination[1]];
     ctx.fillRect(destination[0],destination[1],1,1);
     counter++;
+    counterInput.value = counter;
     if(quantity<counter) clearInterval(triangleInterval);
     
 }   
 
 start = () => {
+    if (isStarted) {
+        return 0;
+    }
+    isStarted = true;
     quantity = document.getElementById('quantity').value;
     triangleInterval = setInterval(drawTriangle,1);
 }
 
 reset = () => {
+    isStarted = false;
     clearInterval(triangleInterval);
     init();
 }
 
 openPage = () => {
     let wrapper = document.getElementById("wrapper");
-    let menuContainer = document.getElementById("menuContainer");
-    let informationContainer = document.getElementById("informationContainer");
+    let leftMenu = document.getElementById("leftMenu");
     let canvasContainer = document.getElementById("canvasContainer");
     wrapper.classList.add("wrapperUp");
-    menuContainer.classList.add("menuAnimation");
-    informationContainer.classList.add("informationAnimation");
+    menuContainer.classList.add("leftMenuAnimation");
     canvasContainer.classList.add("canvasAnimation");
 }
-
-
-
