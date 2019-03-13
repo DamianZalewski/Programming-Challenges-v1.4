0f18edc0447e879231b24b5acf2d2018f81398de
DamianZalewski <damian_zalewskipl@o2.pl>
2019-03-13 20:36:36 +0100

    added words directory, actual word randomize and started key verification.

--- 08-Hangman/scripts/index.js
+++ 08-Hangman/scripts/index.js
@@ -3,18 +3,43 @@ window.onload = function () {
     let ctx = canvas.getContext('2d');
     let cw = canvas.width;
     let ch = canvas.height;
+    let words = ['yell','radiate','slow','ban','thunder','cakes','paint','conscious','nice','room','surround','subdued','hang','icicle','fine','afraid','wide'];
+    let actualWord = '';
+    let hiddenWord = '';
+    let fails = 0;
 
     drawLetters = () => {
         let letterBox = document.getElementsByClassName("lettersBox")[0];
         let letter = 'A';
         for (i = 0; i < 26; i++) {
+            let key = (i + 10).toString(36).toUpperCase();
             var div = document.createElement("div");
             div.classList.add("letter");
-            div.innerHTML = (i + 10).toString(36).toUpperCase();
+            div.innerHTML = key;
+            div.addEventListener('click',() => {checkKey(key)});
             letterBox.appendChild(div);
-
         }
     }
+    
+    drawWord = () => {
+        let wordBox = document.getElementsByClassName("topicWord")[0];
+        wordBox.innerHTML = hiddenWord;
+    }
+    
+    randomizeWord = () => {
+        let i = Math.floor(Math.random() * words.length);
+        actualWord = words[i];
+        for(let i = 0; i < actualWord.length; i++) {
+            hiddenWord += '-';
+        }
+    }
+    
+    checkKey = (key) => {
+        console.log(key);
+    }
+    
     drawLetters();
+    randomizeWord();
+    drawWord();
 
 };
