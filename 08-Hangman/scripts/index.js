217a365fc054fad6f702d5366d1e0c1c385dfd38
DamianZalewski <damian_zalewskipl@o2.pl>
2019-03-17 21:00:09 +0100

    added hangman images plus game over implementation

--- 08-Hangman/scripts/index.js
+++ 08-Hangman/scripts/index.js
@@ -1,8 +1,4 @@
 window.onload = function () {
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-    let cw = canvas.width;
-    let ch = canvas.height;
     let words = ['yell','radiate','slow','ban','thunder','cakes','paint','conscious','nice','room','surround','subdued','hang','icicle','fine','afraid','wide'];
     let actualWord = 'test';
     let hiddenWord = '----';
@@ -15,6 +11,7 @@ window.onload = function () {
             let key = (i + 10).toString(36).toUpperCase();
             var div = document.createElement("div");
             div.classList.add("letter");
+            div.id = 'button_' + key;
             div.innerHTML = key;
             div.addEventListener('click',() => {checkKey(key)});
             letterBox.appendChild(div);
@@ -34,15 +31,36 @@ window.onload = function () {
         }
     }
     
+    changeImage = () => {
+        let hangmanImage = document.getElementsByClassName('hangmanImage')[0];
+        console.log(hangmanImage);
+        hangmanImage.src = 'assets/hangman' + fails + '.png';
+    }
+    
+    removeKey = (key) => {
+        let keyButton = document.getElementById('button_'+key);
+        keyButton.style.visibility = 'hidden';
+    } 
+    
+    setCharAt = (str,index,chr) => {
+        return str.substr(0,index) + chr + str.substr(index+1);
+    }
+    
     checkKey = (key) => {
-        console.log(key);
-        if (actualWord.toUpperCase().indexOf(key) > -1)
-        {
-            let index = actualWord.toUpperCase().indexOf(key);
-//            alert(actualWord.toUpperCase().indexOf(key));
-            hiddenWord = hiddenWord.substr(0, index) + key + hiddenWord.substr(index + 1);
-            console.log(hiddenWord);
+        if(fails == 7) return 0;
+        let changeFlag = false;
+        for(let i = 0; i < actualWord.length; i++) {
+            if(actualWord[i].toUpperCase() == key){
+                hiddenWord = setCharAt(hiddenWord, i, key);
+                changeFlag = true;
+            }
         }
+        if(!changeFlag) {
+            fails++;
+            changeImage();
+        }
+        drawWord();
+        removeKey(key);
     }
     
     drawLetters();
