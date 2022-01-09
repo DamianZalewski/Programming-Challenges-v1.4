1195c54fad08531a2721a2292187982b2ef88899
Damian Zalewski <damian_zalewskipl@o2.pl>
2022-01-09 10:40:12 +0100

    finished calculate age in seconds project

--- 03-Calculate-age-in-seconds/scripts/index.js
+++ 03-Calculate-age-in-seconds/scripts/index.js
@@ -1,92 +1,23 @@
-let inputFahrenheit = document.getElementById('inputFahrenheit');
-let inputRankine = document.getElementById('inputRankine');
-let inputCelsius = document.getElementById('inputCelsius');
-let inputKelvin = document.getElementById('inputKelvin');
+let dateInput = document.querySelector('#dateInput');
+let resultTextError = document.querySelector('#resultTextError');
+let resultText = document.querySelector('#resultText');
 
-function calculateFahrenheit(){
-    let value = inputFahrenheit.value;
-    if(value == null || value == ''|| isNaN(value)){
-        inputFahrenheit.value = "Wrong value of input";
-        inputRankine.value = "Wrong value of input"; 
-        inputCelsius.value = "Wrong value of input"; 
-        inputKelvin.value = "Wrong value of input";
+function calculateAgeInSeconds(){
+    let dateNow = new Date();
+
+    if(dateInput.value == '' || dateInput.value == null){
+        resultTextError.innerText = "fill in the field with the date";
     }else{
-        let fagrenheit = parseFloat(value);
-        let celsius = (fagrenheit - 32) / 1.8;
-        let rankine = (celsius + 273.15) * 1.8;
-        let kelvin = celsius + 273.15;
-  
-        inputFahrenheit.value = Math.round(fagrenheit * 100) / 100;
-        inputRankine.value = Math.round(rankine * 100) / 100;
-        inputCelsius.value = Math.round(celsius * 100) / 100; 
-        inputKelvin.value = Math.round(kelvin * 100) / 100;
+        let dateInputValue = new Date(dateInput.value);
+
+        if(dateNow < dateInputValue){
+            resultTextError.innerText = "Please select a date that is older than today";
+        }else{
+            let resultDate = new Date(dateNow - dateInputValue);
+            resultDate = Math.floor(resultDate.getTime() / 1000);
+
+            resultTextError.innerText = "";
+            resultText.innerText = `From date (${dateInputValue.toLocaleDateString()}) to date (${dateNow.toLocaleDateString()}) is about ${resultDate} seconds`;
+        }
     }
-}
-
-function calculateRankine(){
-    let value = inputRankine.value;
-    if(value == null || value == ''|| isNaN(value)){
-        inputFahrenheit.value = "Wrong value of input";
-        inputRankine.value = "Wrong value of input"; 
-        inputCelsius.value = "Wrong value of input"; 
-        inputKelvin.value = "Wrong value of input";
-    }else{
-        let rankine = parseFloat(value);
-        let celsius = (rankine / 1.8) - 273.15;
-        let fagrenheit = (celsius * 1.8) + 32;
-        let kelvin = celsius + 273.15;
-  
-        inputFahrenheit.value = Math.round(fagrenheit * 100) / 100;
-        inputRankine.value = Math.round(rankine * 100) / 100;
-        inputCelsius.value = Math.round(celsius * 100) / 100; 
-        inputKelvin.value = Math.round(kelvin * 100) / 100;
-    }
-}
-
-function calculateCelsius(){
-    let value = inputCelsius.value;
-    if(value == null || value == ''|| isNaN(value)){
-        inputFahrenheit.value = "Wrong value of input";
-        inputRankine.value = "Wrong value of input"; 
-        inputCelsius.value = "Wrong value of input"; 
-        inputKelvin.value = "Wrong value of input";
-    }else{
-        let celsius = parseFloat(value);
-        let rankine = (celsius + 273.15) * 1.8;
-        let fagrenheit = (celsius * 1.8) + 32;
-        let kelvin = celsius + 273.15;
-  
-        inputFahrenheit.value = Math.round(fagrenheit * 100) / 100;
-        inputRankine.value = Math.round(rankine * 100) / 100;
-        inputCelsius.value = Math.round(celsius * 100) / 100; 
-        inputKelvin.value = Math.round(kelvin * 100) / 100;
-    }
-}
-
-
-function calculateKelvin(){
-    let value = inputKelvin.value;
-    if(value == null || value == ''|| isNaN(value)){
-        inputFahrenheit.value = "Wrong value of input";
-        inputRankine.value = "Wrong value of input"; 
-        inputCelsius.value = "Wrong value of input"; 
-        inputKelvin.value = "Wrong value of input";
-    }else{
-        let kelvin = parseFloat(value);
-        let celsius  = kelvin - 273.15;
-        let rankine = (celsius + 273.15) * 1.8;
-        let fagrenheit = (celsius * 1.8) + 32;
-  
-        inputFahrenheit.value = Math.round(fagrenheit * 100) / 100;
-        inputRankine.value = Math.round(rankine * 100) / 100;
-        inputCelsius.value = Math.round(celsius * 100) / 100; 
-        inputKelvin.value = Math.round(kelvin * 100) / 100;
-    }
-}
-
-function resetForm(){
-    inputFahrenheit.value = "";
-    inputRankine.value = ""; 
-    inputCelsius.value = ""; 
-    inputKelvin.value = "";
 }
